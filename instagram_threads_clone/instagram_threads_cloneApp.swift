be1abe62be87d03673899d339b311defc4d733e7
Onumoh Abubakar <abubakaronumoh@gmail.com>
2024-09-09 09:47:55 +0100

    Initial Commit

---
+++ instagram_threads_clone/instagram_threads_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_threads_cloneApp.swift
+//  instagram_threads_clone
+//
+//  Created by Khalifah on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct instagram_threads_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
